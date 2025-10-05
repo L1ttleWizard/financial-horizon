dc65dfcfb9be4519faf5829ee48f8b85ddd33a74
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-05 23:44:10 +0300

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/financial-horizon' : '',
-  assetPrefix: isProd ? '/financial-horizon/' : '',
-  images:{unoptimized: true}  
+  // basePath: isProd ? '/financial-horizon' : '',
+  // assetPrefix: isProd ? '/financial-horizon/' : '',
+  images:{unoptimized: true},
+  basePath: process.env.PAGES_BASE_PATH, 
+  output:'export'
 };
 
 
