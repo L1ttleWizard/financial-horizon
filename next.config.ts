da400ebdfc3a2044a0e61b1f98ba7043fbe351e2
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-05 23:54:38 +0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   // basePath: isProd ? '/financial-horizon' : '',
   // assetPrefix: isProd ? '/financial-horizon/' : '',
-  images:{unoptimized: true},
   basePath: process.env.PAGES_BASE_PATH, 
 };
 
