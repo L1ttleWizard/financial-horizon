b89968da3b5ac6398e928619acbd09cefbd52439
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-07 01:53:56 +0300

    Revert "Merge branch 'design_overhaul_2' into dev"
    This reverts commit dae38d077ffbb0868eb9f17779ccc442dec4ad56, reversing
    changes made to 45ad99038e9556ab4b79aa30dd35aa94460d6f0e.

--- src/components/ui/DashboardCard.tsx
+++ src/components/ui/DashboardCard.tsx
@@ -1,11 +1,9 @@
 // src/components/ui/DashboardCard.tsx
 import Link from "next/link";
-import React from 'react';
-
 interface CardProps {
   title: string;
   value: string;
-  icon: React.ReactNode;
+  icon: string;
   subValue?: string;
   actionLabel?: string;
   onAction?: () => void;
@@ -33,10 +31,13 @@ export function DashboardCard({
       }
     : {};
 
-  const cardClasses = "rounded-xl p-4 sm:p-6 flex flex-col justify-end h-full";
-
   const content = (
-    <>
+    <div
+      style={cardStyle}
+      className={` rounded-xl   p-4 sm:p-6 flex flex-col justify-end h-full transition-all ${
+        linkTo ? "hover:shadow-lg hover:scale-105" : ""
+      }`}
+    >
       <div className="flex-grow flex justify-between mb-6">
         <div className="flex flex-col justify-center pb-4 sm:pb-6">
           <p className="text-gray-500 text-xs sm:text-sm font-medium uppercase tracking-wider">
@@ -58,24 +59,12 @@ export function DashboardCard({
           {actionLabel}
         </button>
       )}
-    </>
+    </div>
   );
 
   if (linkTo) {
-    return (
-      <Link
-        href={linkTo}
-        style={cardStyle}
-        className={`${cardClasses} transition-all `}
-      >
-        {content}
-      </Link>
-    );
+    return <Link href={linkTo}>{content}</Link>;
   }
 
-  return (
-    <div style={cardStyle} className={cardClasses}>
-      {content}
-    </div>
-  );
+  return content;
 }
