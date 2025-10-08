713b7724449c143653bb0c3e0f269450a4815cdc
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-08 20:23:27 +0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from "next";
-
-
-const nextConfig: NextConfig = {
-
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/financial-horizon",
+  assetPrefix: "/financial-horizon",
+  images: {
+    unoptimized: true,
+  },
 };
 
-
-export default nextConfig;
+module.exports = nextConfig;
