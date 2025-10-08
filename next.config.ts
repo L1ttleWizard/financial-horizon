f2dd74612014fad27c5db618eb348293776c85de
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-08 20:13:38 +0300

    fix yml

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  // basePath: isProd ? '/financial-horizon' : '',
-  assetPrefix: '/financial-horizon/',
-  basePath: process.env.PAGES_BASE_PATH, 
+
 };
 
 
