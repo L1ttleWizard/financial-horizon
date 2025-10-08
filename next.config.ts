2efb81194be826f2dd7a18af00cab1197b3d1944
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-08 20:48:19 +0300

    fix

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: 'https://l1ttlewizard.github.io/financial-horizon/',
+  basePath: 'https://l1ttlewizard.github.io/financial-horizon',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
+
+
