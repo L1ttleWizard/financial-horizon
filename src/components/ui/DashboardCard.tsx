a19fc3094f32f0e8b17a72f3e8b58a8dc695be81
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-10-09 22:17:02 +0300

    ы

--- src/components/ui/DashboardCard.tsx
+++ src/components/ui/DashboardCard.tsx
@@ -1,7 +1,5 @@
 // src/components/ui/DashboardCard.tsx
 import Link from "next/link";
-import React from "react";
-
 interface CardProps {
   title: string;
   value: string;
@@ -41,27 +39,27 @@ export function DashboardCard({
       }`}
     >
       <div className="flex-grow flex justify-between mb-6">
-        <div className=" mt-6  flex flex-col justify-baseline pb-4 sm:pb-6">
+        <div className="flex flex-col justify-center pb-4 sm:pb-6">
           <p className="text-gray-500 text-xs sm:text-sm font-medium uppercase tracking-wider">
             {title}
           </p>
           <p className="text-xl sm:text-2xl font-bold text-gray-800">{value}</p>
           {subValue && <p className="text-xs text-gray-500 mt-1">{subValue}</p>}
-          {actionLabel && (
-            <button
-              onClick={onAction}
-              disabled={actionDisabled}
-              className="mt-4 w-full bg-blue-100 text-blue-700 font-semibold py-2 rounded-lg text-sm hover:bg-blue-200 transition disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed"
-            >
-              {actionLabel}
-            </button>
-          )}
         </div>
-        <div className="flex items-end mb-4">
+        <div className="flex items-end">
           <div className="sm:text-4xl text-6xl">{icon}</div>
         </div>
       </div>
-Z    </>
+      {actionLabel && (
+        <button
+          onClick={onAction}
+          disabled={actionDisabled}
+          className="mt-4 w-full bg-blue-100 text-blue-700 font-semibold py-2 rounded-lg text-sm hover:bg-blue-200 transition disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed"
+        >
+          {actionLabel}
+        </button>
+      )}
+    </div>
   );
 
   if (linkTo) {
