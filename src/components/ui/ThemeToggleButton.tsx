dc829ec29926d4e331e026a290d4104f33974f4f
l1ttlewizard <andrwyandreev2005@gmail.com>
2025-11-06 01:05:13 +0300

    light and dark theme icon

--- src/components/ui/ThemeToggleButton.tsx
+++ src/components/ui/ThemeToggleButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useTheme } from '@/contexts/ThemeContext';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 export const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useTheme();
@@ -8,10 +9,9 @@ export const ThemeToggleButton = () => {
   return (
     <button
       onClick={toggleTheme}
-      className={`p-2 rounded-md text-semibold   ${theme ==='dark'? 'text-gray-300 hover:bg-gray-700': 'text-gray-700 hover:bg-gray-200'}`}
-
+      className={`p-2 rounded-md text-semibold transition-transform duration-300 transform ${theme === 'dark' ? 'text-gray-300 hover:bg-gray-700 rotate-180' : 'text-gray-700 hover:bg-gray-200 rotate-0'}`}
     >
-      {theme === 'light' ? 'Dark' : 'Light'}
+      {theme === 'light' ? <FaMoon /> : <FaSun />}
     </button>
   );
 };
