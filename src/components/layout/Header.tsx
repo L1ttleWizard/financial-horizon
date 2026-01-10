193bb3dde7278bbf80e04f0edf7fde309ca6b3af
l1ttlewizard <andrwyandreev2005@gmail.com>
2026-01-10 06:27:12 +0300

    try to fix loging

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,92 +1,145 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { useAuth } from '@/contexts/AuthContext';
-import { auth } from '@/lib/firebase-client';
-import { signOut } from 'firebase/auth';
-import { useRouter, usePathname } from 'next/navigation';
-import { FaTrophy, FaUser } from 'react-icons/fa';
-import { FiLogIn, FiLogOut } from 'react-icons/fi';
-import { useAppSelector } from '@/store/hooks';
-import { MdAddCircleOutline } from 'react-icons/md';
-import { GoHomeFill } from 'react-icons/go';
-import { GrPowerReset } from 'react-icons/gr';
+import Link from "next/link";
+import { useAuth } from "@/contexts/AuthContext";
+import { auth } from "@/lib/firebase-client";
+import { signOut } from "firebase/auth";
+import { useRouter, usePathname } from "next/navigation";
+import { FaTrophy, FaUser } from "react-icons/fa";
+import { FiLogIn, FiLogOut } from "react-icons/fi";
+import { useAppSelector } from "@/store/hooks";
+import { MdAddCircleOutline } from "react-icons/md";
+import { GoHomeFill } from "react-icons/go";
+import { GrPowerReset } from "react-icons/gr";
 
-import { ThemeToggleButton } from '@/components/ui/ThemeToggleButton';
+import { ThemeToggleButton } from "@/components/ui/ThemeToggleButton";
 
 import { useTheme } from "@/contexts/ThemeContext";
-import { useAppDispatch } from '@/store/hooks';
-import { resetGame } from '@/store/slices/gameSlice';
-import { resetOnboarding, startOnboarding } from '@/store/slices/onboardingSlice';
+import { useAppDispatch } from "@/store/hooks";
+import { resetGame } from "@/store/slices/gameSlice";
+import {
+  resetOnboarding,
+  startOnboarding,
+} from "@/store/slices/onboardingSlice";
 
 export const Header = () => {
   const { theme } = useTheme();
   const { user, loading } = useAuth();
   const router = useRouter();
   const pathname = usePathname();
-  const onboardingCompleted = useAppSelector((state) => state.onboarding.hasCompleted);
+  const onboardingCompleted = useAppSelector(
+    (state) => state.onboarding.hasCompleted
+  );
   const dispatch = useAppDispatch();
 
   const handleLogout = async () => {
     await signOut(auth);
-    router.push('/'); // Redirect to home after logout
+    router.push("/"); // Redirect to home after logout
   };
 
   const handleReset = () => {
-    if (window.confirm('Вы уверены, что хотите начать игру заново? Весь прогресс будет потерян.')) {
+    if (
+      window.confirm(
+        "Вы уверены, что хотите начать игру заново? Весь прогресс будет потерян."
+      )
+    ) {
       dispatch(resetGame());
       dispatch(resetOnboarding());
       dispatch(startOnboarding());
-      router.push('/');
+      router.push("/");
     }
   };
 
+  const renderAuthButtons = () => {
+    if (user) {
+      return (
+        <>
+          <Link
+            href="/profile"
+            className={`text-lg font-medium hover:text-blue-600 transition-colors ${
+              theme === "dark" ? "text-gray-300" : "text-gray-600"
+            }`}>
+            <FaUser />
+          </Link>
+          <button
+            onClick={handleLogout}
+            className=" cursor-pointer text-lg font-medium text-red-600 hover:text-red-800 transition-colors">
+            <FiLogOut />
+          </button>
+        </>
+      );
+    }
+
+    if (loading) {
+      return (
+        <div
+          className={`text-lg font-medium ${
+            theme === "dark" ? "text-gray-300" : "text-gray-600"
+          }`}>
+          Загрузка...
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <Link
+          href="/login"
+          className="text-lg font-medium text-blue-600 hover:text-blue-800 transition-colors">
+          <FiLogIn />
+        </Link>
+        {onboardingCompleted && (
+          <Link
+            href="/register"
+            className="text-lg font-medium text-green-600 hover:text-green-800 transition-colors">
+            <MdAddCircleOutline />
+          </Link>
+        )}
+      </>
+    );
+  };
+
   return (
-    <header className={`w-full shadow-md ${
-      theme === 'dark'
-        ? 'bg-[rgba(34,13,55,0.65)] shadow-[0px_4px_4px_rgba(0,0,0,0.2),_0px_4px_6px_-1px_rgba(0,0,0,0.5),_0px_2px_4px_-2px_rgba(0,0,0,0.1)]'
-        : 'bg-white'
-    }`}>
+    <header
+      className={`w-full shadow-md ${
+        theme === "dark"
+          ? "bg-[rgba(34,13,55,0.65)] shadow-[0px_4px_4px_rgba(0,0,0,0.2),_0px_4px_6px_-1px_rgba(0,0,0,0.5),_0px_2px_4px_-2px_rgba(0,0,0,0.1)]"
+          : "bg-white"
+      }`}>
       <div className="container mx-auto flex justify-between items-center p-4">
-        <Link href="/" className={`text-2xl font-bold hover:text-blue-600 transition-colors ${theme === 'dark' ? 'text-[#E8E1F0]' : 'text-gray-800'}`}>
+        <Link
+          href="/"
+          className={`text-2xl font-bold hover:text-blue-600 transition-colors ${
+            theme === "dark" ? "text-[#E8E1F0]" : "text-gray-800"
+          }`}>
           Финансовый Горизонт
         </Link>
-        <nav className={`flex items-center gap-6 `} >
-          <ThemeToggleButton/>
-          <button onClick={handleReset} className={`text-lg font-medium hover:text-blue-600 transition-colors ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+        <nav className={`flex items-center gap-6 `}>
+          <ThemeToggleButton />
+          <button
+            onClick={handleReset}
+            className={`text-lg font-medium hover:text-blue-600 transition-colors ${
+              theme === "dark" ? "text-gray-300" : "text-gray-600"
+            }`}>
             <GrPowerReset />
           </button>
-          {pathname !== '/' && (
-            <Link href="/" className={`text-lg font-medium hover:text-blue-600 transition-colors ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+          {pathname !== "/" && (
+            <Link
+              href="/"
+              className={`text-lg font-medium hover:text-blue-600 transition-colors ${
+                theme === "dark" ? "text-gray-300" : "text-gray-600"
+              }`}>
               <GoHomeFill />
             </Link>
           )}
-          <Link href="/leaderboard" className={`text-lg font-medium hover:text-blue-600 transition-colors ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
+          <Link
+            href="/leaderboard"
+            className={`text-lg font-medium hover:text-blue-600 transition-colors ${
+              theme === "dark" ? "text-gray-300" : "text-gray-600"
+            }`}>
             <FaTrophy />
           </Link>
-          {loading ? (
-            <div className={`text-lg font-medium ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>Загрузка...</div>
-          ) : user ? (
-            <>
-              <Link href="/profile" className={`text-lg font-medium hover:text-blue-600 transition-colors ${theme === 'dark' ? 'text-gray-300' : 'text-gray-600'}`}>
-                <FaUser />
-              </Link>
-              <button onClick={handleLogout} className=" cursor-pointer text-lg font-medium text-red-600 hover:text-red-800 transition-colors">
-                <FiLogOut />
-              </button>
-            </>
-          ) : (
-            <>
-              <Link href="/login" className="text-lg font-medium text-blue-600 hover:text-blue-800 transition-colors">
-              <FiLogIn />
-              </Link>
-              {onboardingCompleted && (
-                <Link href="/register" className="text-lg font-medium text-green-600 hover:text-green-800 transition-colors">
-                  <MdAddCircleOutline />
-                </Link>
-              )}
-            </>
-          )}
+          {renderAuthButtons()}
         </nav>
       </div>
     </header>
